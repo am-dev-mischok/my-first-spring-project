d1215fb9befb810452b6c14a5cabab9616d87a53
Alexander Mai <alexander.mai@mischok.de>
2024-09-13 11:42:30 +0200

    implement simple controller with boring thymeleaf file

---
+++ src/main/java/com/academy/my_first_spring_project/GreetingController.java
@@ -0,0 +1,14 @@
+package com.academy.my_first_spring_project;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class GreetingController {
+
+    @GetMapping("/greeting")
+    public String greeting(Model model) {
+        return "greeting";
+    }
+}
