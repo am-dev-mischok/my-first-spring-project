bdff6f01f64e45d162ba998d803528d30dc9f524
Alexander Mai <alexander.mai@mischok.de>
2024-09-13 14:44:10 +0200

    add RequestParam as variable to endpoint, insert in Thymeleaf template

--- src/main/java/com/academy/my_first_spring_project/GreetingController.java
+++ src/main/java/com/academy/my_first_spring_project/GreetingController.java
@@ -3,12 +3,21 @@ package com.academy.my_first_spring_project;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class GreetingController {
 
     @GetMapping("/greeting")
-    public String greeting(Model model) {
+    public String greeting(
+            @RequestParam(name="n", required = false, defaultValue = "Welt") String someName,
+            Model model
+    ) {
+        model.addAttribute("inputName", someName);
         return "greeting";
     }
 }
