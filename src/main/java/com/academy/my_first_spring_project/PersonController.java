388c4d2b30292bde4769defc34c31ba068b8671c
Alexander Mai <alexander.mai@mischok.de>
2024-09-24 10:40:08 +0200

    implement current state of README tutorial

--- src/main/java/com/academy/my_first_spring_project/PersonController.java
+++ src/main/java/com/academy/my_first_spring_project/PersonController.java
@@ -4,8 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
@@ -19,34 +19,25 @@ public class PersonController {
     @GetMapping(value = "/person")
     @ResponseBody
     public List<Person> getPersons() {
-        List<Person> persons = personRepository.findAll();
-        return persons;
+        return personRepository.findAll();
     }
 
-    @PostMapping(value = "/person", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "/person/{pId}")
     @ResponseBody
-    public Person createPerson(@RequestBody Person person) throws RuntimeException {
-        // wir sollten checken, dass das neu zu erstellende Objekt keine id hat, denn diese wird automatisch vergeben von JPA
-        if (person.getId() != null) {
-            throw new RuntimeException("person is not allowed to have an id");
-        }
-
-        Person savedPerson = personRepository.save(person);
-        return savedPerson;
+    public Person getPersonById(
+            @PathVariable(name="pId") Long personId
+    ) {
+        return personRepository.findById(personId)
+                .orElseThrow();
     }
 
     @PostMapping(value = "/person", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
-    public String createPersonFromForm(Person person) {
-        // hier sollten wir die Person speichern, aber wir haben noch keine Datenbank
-        System.out.println(person.getId());
-        System.out.println(person.getName());
-        System.out.println(person.getEmail());
-        System.out.println(person.getAge());
-        System.out.println(person.getMarried());
-
-        // wir leiten den User als Antwort einfach auf die Hauptseite zurück
-        return "redirect:/";
-
-        // stattdessen könnten wir den Namen der Person auch mit model.addAttribute("inputName", person.name()); für Thymeleaf bereitstellen und dann unser greeting.html Template zurückgeben
+    @ResponseBody
+    public Person createPersonFromForm(Person person) {
+        if (person.getId() != null) {
+            throw new RuntimeException("new person is not allowed to have an id already; let the database give an available id");
+        }
+        Person personWithId = personRepository.save(person);
+        return personWithId;
     }
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,10 +1,10 @@
---Die SQL-Queries in dieser Datei werden von Spring beim Start des Programms automatisch ausgeführt, wenn wir H2 im In-Memory-Mode laufen lassen. Oder auch wenn wir im File-Mode sind, aber dann in den application.properties setzen: #spring.sql.init.mode=always
---Wir erstellen unsere Tabellen stattdessen mit Flyway
+--Die SQL-Queries in dieser Datei werden von Spring beim Start des Programms automatisch ausgeführt, wenn wir H2 im In-Memory-Mode laufen lassen. Oder auch wenn wir im File-Mode sind, aber dann in den application.properties setzen:   spring.sql.init.mode=always
+--Das machen wir aber lieber nicht und erstellen unsere Tabellen stattdessen mit Flyway.
 
 CREATE TABLE IF NOT EXISTS person (
     id BIGSERIAL PRIMARY KEY,
-    name varchar(50),
-    email varchar(50),
-    age int,
-    married boolean
+    name VARCHAR(255),
+    email VARCHAR(255),
+    age INT,
+    married BOOLEAN
 );
